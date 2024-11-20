a661e9442fdfda04241a2a0d6bded4a6649b9a94
nishantsaini55 <nishantsaini5556@gmail.com>
2024-11-20 09:01:18 +0530

    defined a key_generator function for user authentication

--- Automating Cybersecurity Tasks Mini Projects/Password_generator.py
+++ Automating Cybersecurity Tasks Mini Projects/Password_generator.py
@@ -10,25 +10,32 @@ class Password:
 
     @staticmethod
     def input_name():
-        name = input("ENTER YOUR NAME: ")
+        name = input("Enter your Username : ")
         return name
 
     @staticmethod
     def input_pass():
-        passw = getpass.getpass("ENTER YOUR PASSWORD: ")
-        print(f"before hashing: {passw}")
+        passw = getpass.getpass("Enter your password : ")
         return passw
 
     def generator(self) -> str:
         char_list = string.ascii_letters + string.digits + string.punctuation
         password = self.password + ''.join(random.choice(char_list) for _ in range(12 - len(self.password)))
-        print(f"after hashing: {password}")
+        print(f"Your password is generated successfully ")
         return password
+    
+    def key_generator(self) :
+         char_list = string.ascii_letters + string.digits 
+         key = random.choice(char_list)
+         print(f"Your key to access your password is {key}")
+         return key
+
 
 
 name = Password.input_name()
 passw = Password.input_pass()
 obj1 = Password(name, passw)
 hashed_password = obj1.generator()
+key = obj1.key_generator()
 
     
