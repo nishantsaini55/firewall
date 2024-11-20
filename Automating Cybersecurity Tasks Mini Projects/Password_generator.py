6f47b5b6389a353dbddad5c498ffbbc2a43a6b78
nishantsaini55 <nishantsaini5556@gmail.com>
2024-11-20 08:50:13 +0530

    deleted the app.log and readme.md of previous project

--- Automating Cybersecurity Tasks Mini Projects/Password_generator.py
+++ Automating Cybersecurity Tasks Mini Projects/Password_generator.py
@@ -1,20 +1,7 @@
 
-import logging
 import random
 import string
 import getpass
-
-# Configure logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler('app.log'),
-        logging.StreamHandler()
-    ]
-)
-
-logger = logging.getLogger('Nanu')
 
 class Password:
     def __init__(self, name=None, password=None):
@@ -30,15 +17,12 @@ class Password:
     def input_pass():
         passw = getpass.getpass("ENTER YOUR PASSWORD: ")
         print(f"before hashing: {passw}")
-
-        logger.info(f"Password without hashing is: {passw}")
         return passw
 
     def generator(self) -> str:
         char_list = string.ascii_letters + string.digits + string.punctuation
-        password = self.password + ''.join(random.choice(char_list) for _ in range(12 - len(self.password)))  # Assuming a total length of 12 characters
+        password = self.password + ''.join(random.choice(char_list) for _ in range(12 - len(self.password)))
         print(f"after hashing: {password}")
-        logger.critical(f"hashed password is: {password}")
         return password
 
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# firewall
-This is a personal project-based Firewall developed majorly using python and its refined libraries. I hope you like it  :-) .
